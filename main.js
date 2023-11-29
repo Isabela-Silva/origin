103a1385f0f49f8b2332d23990b4108be3c76f52
isabela silva <isabelabaia123@gmail.com>
2023-11-29 12:40:04 -0300

    making desktop

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector(".container");
+const nav = document.querySelector(".container")
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
@@ -14,18 +14,20 @@ for (const link of links) {
   })
 }
 
-/* mudar o header da pagina quando der scroll*/
-
+// mudar o header da pagina quando der scroll
 const header = document.querySelector('#header')
 const altHeader = header.offsetHeight
 
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScroll() {
+
   if (window.scrollY >= altHeader) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
+
+
 
 // carroussel swiper
 const swiper = new Swiper('.swiper', {
@@ -34,7 +36,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 // scroll reveal
@@ -51,7 +59,63 @@ scrollReveal.reveal(
   #about .image, #about .texts,
   #services header, #services .card,
   #testimonials header, #testimonials .swiper-wrapper,
-  #contact .texts, #contact .links
+  #contact .texts, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100}
 )
+// button to return to top 
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if(window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for( const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd) {
+      document.querySelector('nav ul li a[href*=' + sectionId +']')
+      .classList.add('active')
+    } else {
+      document.querySelector('nav ul li a[href*=' + sectionId +']')
+      .classList.remove('active')
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// when scroll
+window.addEventListener('scroll', () => {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
+
